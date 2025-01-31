05a2e31fdd1c86b907c61daf2bd40c32ea58a116
Marine-Tiger <120460845+Marine-Tiger@users.noreply.github.com>
2025-01-31 15:10:26 +0100

    create results and cleaned files

--- JSONtoCSV.py
+++ JSONtoCSV.py
@@ -2,7 +2,7 @@ import json
 import csv
 import pandas as pd
 
-with open('results.json', encoding='utf-8') as inputfile:
+with open('./RESULTS/results.json', encoding='utf-8') as inputfile:
     df = pd.read_json(inputfile)
 
-df.to_csv('results.csv', encoding='utf-8', index=False)
+df.to_csv('./RESULTS/results.csv', encoding='utf-8', index=False)
